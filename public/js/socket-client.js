96529e9f80a80e424c67d7e1d00f697521038e2b
davidmoina <luisdavidmoina@gmail.com>
2023-06-12 20:59:44 +0200

    creating websocket server

---
+++ public/js/socket-client.js
@@ -0,0 +1,40 @@
+// referencias del html
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+const txtMessage = document.querySelector('#txtMessage');
+const btnSend = document.querySelector('#btnSend');
+
+const socket = io();
+
+socket.on('connect', () => {
+	// console.log('Conectado');
+
+	lblOffline.style.display = 'none';
+	lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+	// console.log('Desconectado del servidor');
+
+	lblOnline.style.display = 'none';
+	lblOffline.style.display = '';
+});
+
+socket.on('send-message', payload => {
+	// console.log('Desconectado del servidor');
+
+	console.log(payload);
+});
+
+btnSend.addEventListener('click', () => {
+	const message = txtMessage.value;
+	const payload = {
+		message,
+		id: 'abc123',
+		date: new Date().getTime(),
+	};
+
+	socket.emit('send-message', payload, id => {
+		console.log('Desde el server ', id);
+	});
+});
